ba8b9e4c88b0ac59f3b66c26b346db07045b54fd
Albert Muhatia <albertmuhatia@gmail.com>
2023-08-10 15:49:24 +0300

    Update R.E Compyright

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1081,14 +1081,27 @@
             <div class="col-lg-6 m-auto wow fadeInUp" data-wow-delay="00ms">
               <div class="main-title text-start text-md-center">
                 <h2 class="title">From Our Blog</h2>
-                <p class="paragraph">Aliquam lacinia diam quis lacus euismod</p>
+                <p class="paragraph">Learn From Oyr Random Real Estate Articles</p>
               </div>
             </div>
           </div>
           <div class="row wow fadeInUp" data-wow-delay="300ms">
             <div class="col-sm-6 col-lg-4">
               <div class="blog-style1">
-                <div class="blog-img"><img class="w-100" src="images/blog/blog-1.jpg" alt=""></div>
+                <div class="blog-img"><img class="w-100" src="{{asset('theme/images/blog/blog-1.jpg')}}" alt=""></div>
+                <div class="blog-content">
+                  <div class="date">
+                    <span class="month">June</span>
+                    <span class="day">18</span>
+                  </div>
+                  <a class="tag" href="#">Living Room</a>
+                  <h6 class="title mt-1"><a href="page-blog-single.html">Private Contemporary Home Balancing Openness</a></h6>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+              <div class="blog-style1">
+                <div class="blog-img"><img class="w-100" src="{{asset('theme/images/blog/blog-2.jpg')}}" alt=""></div>
                 <div class="blog-content">
                   <div class="date">
                     <span class="month">July</span>
@@ -1101,20 +1114,7 @@
             </div>
             <div class="col-sm-6 col-lg-4">
               <div class="blog-style1">
-                <div class="blog-img"><img class="w-100" src="images/blog/blog-2.jpg" alt=""></div>
-                <div class="blog-content">
-                  <div class="date">
-                    <span class="month">July</span>
-                    <span class="day">28</span>
-                  </div>
-                  <a class="tag" href="#">Living Room</a>
-                  <h6 class="title mt-1"><a href="page-blog-single.html">Private Contemporary Home Balancing Openness</a></h6>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-6 col-lg-4">
-              <div class="blog-style1">
-                <div class="blog-img"><img class="w-100" src="images/blog/blog-3.jpg" alt=""></div>
+                <div class="blog-img"><img class="w-100" src="{{asset('theme/images/blog/blog-3.jpg')}}" alt=""></div>
                 <div class="blog-content">
                   <div class="date">
                     <span class="month">July</span>
@@ -1235,7 +1235,7 @@
         <div class="row">
           <div class="col-sm-6">
             <div class="text-center text-lg-start">
-              <p class="copyright-text ff-heading mb-0">© Marlene Real Estate - All rights reserved</p>
+              <p class="copyright-text ff-heading mb-0">Copyright © Marlene Real Estate {{date('Y')}} - All rights reserved</p>
             </div>
           </div>
           <div class="col-sm-6">
