ed17477f647a2ead79e6e73fcb28cbc457d99fa2
Albert Muhatia <albertmuhatia@gmail.com>
2023-08-11 17:20:33 +0300

    Update Home

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -648,12 +648,12 @@
                         <div class="col-lg-3">
                           <div class="dark-light-navtab style2 text-start text-lg-end mt-0 mt-lg-4 mb-4">
                             <ul class="nav nav-pills justify-content-start justify-content-lg-end" id="pills-tab" role="tablist">
-                              <li class="nav-item" role="presentation">
+                              {{-- <li class="nav-item" role="presentation">
                                 <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">For Rent</button>
                               </li>
                               <li class="nav-item" role="presentation">
                                 <button class="nav-link me-0" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">For Sale</button>
-                              </li>
+                              </li> --}}
                             </ul>
                           </div>
                         </div>
